dcb789600d816c9fb7550f54c8d1b6f5b4be7e35
seanyushoumori <124742441+seanyushoumori@users.noreply.github.com>
2024-01-29 15:07:01 -0800

    Add files via upload

--- XMAIN.sh
+++ XMAIN.sh
@@ -3,5 +3,5 @@ rm -r cexts
 mkdir -p cexts/crxs cexts/zips cexts/unzips
 mkdir -p results
 node make_proxies.js
-node process_extensions.js
+#node process_extensions.js
 node test_extensions_framework.js
